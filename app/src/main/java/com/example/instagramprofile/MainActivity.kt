8cac5f4dfd4b17fe4de332e9b2b524413dae8d77
esipiderman <sobiesipiderman@gmail.com>
2022-08-27 22:31:25 +0430

    initial commit

---
+++ app/src/main/java/com/example/instagramprofile/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.instagramprofile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.instagramprofile.ui.theme.InstagramProfileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InstagramProfileTheme {
+                ProfileScreen()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/instagramprofile/StoryHighLight.kt
@@ -0,0 +1,10 @@
+package com.example.instagramprofile
+
+import androidx.compose.ui.graphics.painter.Painter
+
+data class StoryHighLight(
+
+    val image : Painter,
+    val text:String
+
+)
